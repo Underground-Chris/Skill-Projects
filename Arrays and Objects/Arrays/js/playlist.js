07b3fd0c35bc8abb31e7f5559e6b3335d4e09070
Chris Mennens <Chrismennens@gmail.com>
2019-07-08 19:05:33 -0400

    Arrays
    Added Arrays and Objects area for course work, cleaned up base folder.

---
+++ Arrays and Objects/Arrays/js/playlist.js
@@ -0,0 +1,11 @@
+//Chris Mennens 7/8/2019
+
+//Setting an Array Literal = [] (Nothing in it)
+var playList = [];
+// Note: Push adds to the end of an array.
+//    Unshift adds to the beginning of an array.
+
+playList.push('Dreams', 'Rebel Heart', 'Imagine');
+playList.unshift('Born to Run');
+playList.unshift('Rebel Yell', 'Landslide');
+printList(playList);
